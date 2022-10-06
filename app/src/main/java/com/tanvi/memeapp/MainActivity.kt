2e3b72f8eba295b3acd7326e674827a4374848b4
tanvi1017 <tripathi.tanvi1996@gmail.com>
2022-10-06 21:10:21 +0530

    Code cleanup

--- app/src/main/java/com/tanvi/memeapp/MainActivity.kt
+++ app/src/main/java/com/tanvi/memeapp/MainActivity.kt
@@ -2,18 +2,15 @@ package com.tanvi.memeapp
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -21,8 +18,8 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 
- class MainActivity : AppCompatActivity() {
-    var currentImageUrl:String?=null
+class MainActivity : AppCompatActivity() {
+    var currentImageUrl: String? = null
     lateinit var progressBar: ProgressBar
     lateinit var imageView: ImageView
     lateinit var btnNextMeme: Button
@@ -30,14 +27,13 @@ import com.bumptech.glide.request.target.Target
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnNextMeme =findViewById(R.id.btnNextMeme)
-        btnShareMeme =findViewById(R.id.btnShareMeme)
+        btnNextMeme = findViewById(R.id.btnNextMeme)
+        btnShareMeme = findViewById(R.id.btnShareMeme)
         imageView = findViewById(R.id.memeImageView)
         progressBar=findViewById(R.id.progressBar)
         loadMeme()
     }
-    fun initializeView(){
-    }
+
     private fun loadMeme() {
         progressBar.visibility = View.VISIBLE
         val queue = Volley.newRequestQueue(this)
@@ -75,34 +71,22 @@ import com.bumptech.glide.request.target.Target
                 Toast.makeText(this,"Something went wrong",Toast.LENGTH_LONG).show()
             })
 
-// Add the request to the RequestQueue.
         queue.add(jsonObjectRequest)
     }
 
-/*
-    private fun loadMeme() {
-        val queue = Volley.newRequestQueue( this)
-        val url = "https://www.google.com"
-        val stringRequest = StringRequest(Request.Method.GET, url,
-            Response.Listener<String> { response ->
-              //  tex.text = "Response is: ${response.substring(0, 500)}"
-            },
-          //  Response.ErrorListener { textView.text = "That didn't work!" })
-        queue.add(stringRequest)
-    }
-    */
-
-    fun shareMeme(view: View){
+    fun shareMeme() {
         val intent = Intent(Intent.ACTION_SEND)
-        intent.type="text/plain"
-        intent.putExtra(Intent.EXTRA_TEXT,"Hey,Checkout this cool meme I gotfree from Reddit $currentImageUrl")
-        val chooser = Intent.createChooser(intent,"share this meme using...")
+        intent.type = "text/plain"
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Hey,Checkout this cool meme I gotfree from Reddit $currentImageUrl"
+        )
+        val chooser = Intent.createChooser(intent, "share this meme using...")
         startActivity(chooser)
     }
-    fun nextMeme(view: View){
 
+    fun nextMeme() {
         loadMeme()
-
     }
 }
 
